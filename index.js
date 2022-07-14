f1a87551a6f38343abbb4d06ce263c3f01dec906
Dobroskokwork <105386218+Dobroskokwork@users.noreply.github.com>
2022-07-14 16:33:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const carName = 'Porsche';
+let maxCarSpeed = '300';
+let carOwner = 'Петя Пяточкин';
+
+maxCarSpeed = '150';
+console.log( 'У машины изменилась скорость ', maxCarSpeed)
+maxCarSpeed = '425'
+console.log( 'У машины изменилась скорость ', maxCarSpeed)
+maxCarSpeed = '500'
+console.log( 'У машины изменилась скорость ', maxCarSpeed) 
+carOwner = 'Владимир Владимирович';
+console.log('У машины изменился владелец', carOwner)
+carOwner = 'Иван Иванов';
+console.log('У машины изменился владелец', carOwner)
+carOwner = 'Петя Петров';
+console.log('У машины изменился владелец', carOwner)
